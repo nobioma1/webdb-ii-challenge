d9dc2310622c34cb130b42466210b94f1085d626
Noble Obioma <nobioma1@gmail.com>
2019-07-16 14:56:32 +0100

    Create query builder to post car and get by id

--- api/cars/carsDbHelper.js
+++ api/cars/carsDbHelper.js
@@ -1,9 +1,20 @@
 const db = require('../../data/dbConfig');
 
-module.exports = {
-  get,
-};
+module.exports = { get, add };
 
 function get() {
   return db('cars');
+}
+
+function getById(id) {
+  return db('cars')
+    .where({ id })
+    .first();
+}
+
+function add(car) {
+  console.log(car)
+  return db('cars')
+    .insert(car)
+    .then(([id]) => getById(id))
 }
