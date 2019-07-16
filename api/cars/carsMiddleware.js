8011a6ff08ad53b729b8a047206af01377c7b9cf
Noble Obioma <nobioma1@gmail.com>
2019-07-16 15:09:46 +0100

    Create middleware to validate car id

--- api/cars/carsMiddleware.js
+++ api/cars/carsMiddleware.js
@@ -1,3 +1,5 @@
+const { getById } = require('./carsDbHelper');
+
 const validateFields = body => {
   const vin = body.hasOwnProperty('vin');
   const make = body.hasOwnProperty('make');
@@ -17,4 +19,17 @@ const validateCar = async (req, res, next) => {
   }
 };
 
-module.exports = { validateCar };
+const validateCarId = async (req, res, next) => {
+  try {
+    const car = await getById(req.params.id);
+    if (!car) {
+      res.status(400).json({ error: 'car id is not Valid' });
+    }
+    req.car = car;
+    return next();
+  } catch (error) {
+    res.status(500).json({ error: 'Error Validating car Id' });
+  }
+};
+
+module.exports = { validateCar, validateCarId };
